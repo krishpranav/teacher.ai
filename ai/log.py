5394b280016a75ac013f32484ef9b2c900edb6b5
krishpranav <krisna.pranav@gmail.com>
2023-07-28 21:46:09 +0530

    ai: log[logger]

--- ai/log.py
+++ ai/log.py
@@ -6,11 +6,22 @@
 
 import logging
 
-def get_logging(logger_name):
+# basic formatter
+formatter = '%(asctime)s - %(funcName)s - %(filename)s - %(levelname)s - %(message)s'
+
+
+def get_logger(logger_name):
     logger = logging.getLogger(logger_name)
     logger.setLevel(logging.DEBUG)
-    
+
+    # console_handler
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.DEBUG)
+    
+    ch_format = logging.Formatter(formatter)
+    
+    console_handler.setFormatter(ch_format)
+
     logger.addHandler(console_handler)
+
     return logger
