292a21db689490bb4b1d67f34a93a604f1fa0520
krishpranav <krisna.pranav@gmail.com>
2023-07-28 21:53:19 +0530

    ai: models[interaction]

--- ai/models/interaction.py
+++ ai/models/interaction.py
@@ -5,18 +5,34 @@
 '''
 
 import datetime
-from ai.database.base import Base
 from sqlalchemy import Column, Integer, String, DateTime, Unicode
+from ai.database.base import Base
 
-class Interactor(Base):
+
+class Interaction(Base):
+    # table_name
     __tablename__ = "interactions"
-    
+
+    # id
     id = Column(Integer, primary_key=True, index=True, nullable=False)
-    client_id = Column(Integer)
+    
+    # id[client, user, session]
+    client_id = Column(Integer)  
     user_id = Column(String(50))
     session_id = Column(String(50))
-    client_message = Column(String)
     
+    # client_message + server_message
+    client_message = Column(String)
+    server_message = Column(String)
+    client_message_unicode = Column(Unicode(65535))
+    server_message_unicode = Column(Unicode(65535))
+
+    timestamp = Column(DateTime, default=datetime.datetime.utcnow)
+    platform = Column(String(50))
+    action_type = Column(String(50))
+    character_id = Column(String(100))
+    tools = Column(String(100))
+
     def save(self, db):
         db.add(self)
         db.commit()
