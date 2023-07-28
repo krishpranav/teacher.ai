1109687f9ab948ed462991e158e48b32c1f9ab64
krishpranav <krisna.pranav@gmail.com>
2023-07-28 21:49:31 +0530

    ai: models[usermodel]

--- ai/models/usermodel.py
+++ ai/models/usermodel.py
@@ -4,15 +4,17 @@
 @copyright: 2023 Krisna Pranav MIT License
 '''
 
-from sqlalchemy import Integer, String, Column
 from ai.database.base import Base
+from sqlalchemy import Column, Integer, String
+
 
 class User(Base):
     __tablename__ = "users"
-    
+
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    
+    email = Column(String, unique=True, index=True, nullable=False)
+
     def save(self, db):
         db.add(self)
         db.commit()
