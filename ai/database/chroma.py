b17416369e221e9968401bac08d6f49a824611be
krishpranav <krisna.pranav@gmail.com>
2023-07-28 22:05:14 +0530

    ai: database[chroma]

--- ai/database/chroma.py
+++ ai/database/chroma.py
@@ -10,15 +10,21 @@ from langchain.vectorstores import Chroma
 from langchain.embeddings import OpenAIEmbeddings
 from ai.log import get_logger
 
+# load.
 load_dotenv()
 
 # logger
 logger = get_logger(__name__)
 
-# embedding
+# embed OpenAIEmbeddings
 embedding = OpenAIEmbeddings(openai_api_key=os.getenv("OPENAI_API_KEY"))
 
-# get chroma
+# OPENAI_API_TYPE azure.
+if os.getenv('OPENAI_API_TYPE') == 'azure':
+    embedding = OpenAIEmbeddings(openai_api_key=os.getenv("OPENAI_API_KEY"), deployment=os.getenv(
+        "OPENAI_API_EMBEDDING_DEPLOYMENT_NAME", "text-embedding-ada-002"), chunk_size=1)
+
+# get_chroma
 def get_chroma():
     chroma = Chroma(
         collection_name='llm',
