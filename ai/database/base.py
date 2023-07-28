b00cfdbc9dfc8d6b9f557e7ec5b01d44bc55fc95
krishpranav <krisna.pranav@gmail.com>
2023-07-28 21:34:48 +0530

    ai: database[base]

---
+++ ai/database/base.py
@@ -0,0 +1,10 @@
+'''
+@filename: base.py
+@author: krisna pranav
+@copyright: 2023 Krisna Pranav MIT License
+'''
+
+from sqlalchemy.ext.declarative import declarative_base
+
+# declarative base defined into => Base.
+Base = declarative_base()
