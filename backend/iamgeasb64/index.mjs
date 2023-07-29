0b2328a37d3fae56181a4a4b414211de6e020fab
krishpranav <krisna.pranav@gmail.com>
2023-07-29 07:15:28 +0530

    image as base64 index

--- backend/iamgeasb64/index.mjs
+++ backend/iamgeasb64/index.mjs
@@ -1,34 +1,62 @@
-import http from 'http';
 import https from 'https';
+import http from 'http';
 
 export async function handler(event) {
-    const imageUrl = event.queryStringParameters?.imageUrl;
+  const imageUrl = event.queryStringParameters?.imageUrl;
 
-    const headers = {
-        // 
-    }
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'POST',
+    'Access-Control-Allow-Headers': 'Content-Type'
+  };
 
-    if (!imageUrl) {
-        return {
-            statusCode: 400,
-            headers,
-            body: JSON.stringify({
-                success: false,
-                message: 'Image URL params',
-            }),
-        };
-    }
+  if (!imageUrl) {
+    return {
+      statusCode: 400,
+      headers,
+      body: JSON.stringify({
+        success: false,
+        message: 'MSG: Image URL parameter is required',
+      }),
+    };
+  }
 
-    const httpModule = imageUrl.startsWith('https') ? https : http;
+  const httpModule = imageUrl.startsWith('https') ? https : http;
 
-    try {
-        const base64Image = await new Promise((resolve, reject) => {
-            httpModule
-        })
-    } catch (error) {
-        console.error('Error fetching IMAGE', error);
-        return {
-            statusCode: 500
+  try {
+    const base64Image = await new Promise((resolve, reject) => {
+      httpModule.get(imageUrl, (res) => {
+        if (res.statusCode !== 200) {
+          reject(new Error(`ERROR: Request failed with status code ${res.statusCode}`));
+          return;
         }
-    }
+
+        const chunks = [];
+        res.on('data', (chunk) => chunks.push(chunk));
+        res.on('end', () => {
+          const buffer = Buffer.concat(chunks);
+          const base64 = buffer.toString('base64');
+          resolve(base64);
+        });
+      }).on('error', (err) => {
+        reject(err);
+      });
+    });
+
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify({ success: true, base64Image }),
+    };
+  } catch (error) {
+    console.error('ERROR: Error fetching image:', error);
+    return {
+      statusCode: 500,
+      headers,
+      body: JSON.stringify({
+        success: false,
+        message: `ERROR: Failed to fetch image: ${error.message}`,
+      }),
+    };
+  }
 }
