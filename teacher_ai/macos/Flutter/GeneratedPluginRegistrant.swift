5924057166685a8b822689f367218ffd61608f92
krishpranav <krisna.pranav@gmail.com>
2023-07-29 07:19:52 +0530

    add flutter project

---
+++ teacher_ai/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,18 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_secure_storage_macos
+import flutter_tts
+import shared_preferences_foundation
+import url_launcher_macos
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+}
